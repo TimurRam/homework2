731d152a6cc550a06b4367ce6f0e27880ee0b503
Timur Ramazanov <Sieger.07@mail.ru>
2023-01-20 20:47:28 +0300

    homework 2 first attempt

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 // Задание 1 
+let a = 10; 
+alert(a);
+a = 20;
+alert(a);
+// Задание 2
+const year = 2007;
+alert(`Год выпуска первого Iphone ${year}`);
+// Задание 3\
+let man = 'Brendon Eich';
+alert(`Имя создателя языка JavaScript ${man}`);
+// Задание 4
+let x = 10;
+let y = 2;
+alert(x + y);
+alert(x - y);
+alert(x * y);
+alert(x / y);
+// Задание 5
+let number = 2;
+let result = 2**5;
+alert(result);
+// Задание 6
+let b = 9;
+let c = 2;
+alert(b%c);
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+--num
+++num
+alert (num);
+// Задание 8 
+let age = prompt('Сколько Вам лет?');
+let total = age;
+alert(`Вам ${total} лет!`);
+// Задание 9
+const user ={
+    name: 'Вася',
+    age: 25,
+    isAdmin: true
+};
+user["city of residence"] = 'Пенза';
+user.age = 30;
+delete user["city of residence"];
+let info = prompt('Какую информацию хотите узнать о пользователе?');
+alert(user[info]);
+// Задание 10
+let userName = prompt('Как ваше имя?');
+ alert(`Привет, ${userName}!`);
+
+
